adea2fb163b1e7ce0b5297be32ff7d0daf262d1c
Kamal <kamaldeep734833@gmail.com>
2025-01-13 22:45:27 +0530

    update some packege

--- src/main/java/com/Banking/Application/Banking/Application/Config/SecurityConfig.java
+++ src/main/java/com/Banking/Application/Banking/Application/Config/SecurityConfig.java
@@ -1,14 +1,9 @@
 package com.Banking.Application.Banking.Application.Config;
 
-import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 public class SecurityConfig {
     @Bean
